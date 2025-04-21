a261057bc48a0cc858d8955b8f0643f63503c5f4
Przemek Lewtak <plewtak@insights.com>
2025-04-21 07:36:13 +0100

    Added fix for 'barell' import/export for StoreProvider

--- src/shared/providers/StoreProvider.tsx
+++ src/shared/providers/StoreProvider.tsx
@@ -4,6 +4,8 @@ import { store } from "@/app";
 
 type StoreProviderProps = BaseProps<React.JSX.Element | React.JSX.Element[]>;
 
-export default function StoreProvider({ children }: StoreProviderProps) {
+function StoreProvider({ children }: StoreProviderProps) {
 	return <Provider store={store}>{children}</Provider>;
 }
+
+export { StoreProvider };
